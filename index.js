34a2e2199a59bf8fb5394308e2cc310c1efcd58a
unknown <shaimazerin46@gmail.com>
2025-02-04 20:52:55 +0600

    pacakges api created

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const dbCollection = client.db('hostelManagement')
+
+    const mealsCollection = dbCollection.collection('meals');
+    const packageCollection = dbCollection.collection('packeges');
+
+    // meals API
+    app.get('/meals', async (req,res)=>{
+        const result = await mealsCollection.find().toArray();
+        res.send(result)
+    })
+
+    // Packages API
+    app.get('/packages', async (req,res)=>{
+      const result = await packageCollection.find().toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
